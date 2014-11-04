24b915971f79d95814cf5b9d5cf5fc8b9b2e4cad
Happuri <happuri@gmx.com>
2014-11-04 12:42:44 +0100

    soundsimple draft

---
+++ soundsimple/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lsfml-graphics  -lutil -lboost_iostreams -lboost_system  -lboost_filesystem -lm -lfftw3 -lsfml-audio -lsfml-window  -lsfml-system
+
