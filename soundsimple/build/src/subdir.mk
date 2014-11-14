2c5f0f5512d179831e29763400489351fe1c1490
Happuri <happuri@gmx.com>
2014-11-14 10:51:30 +0100

    New eclipse project

--- soundsimple/build/src/subdir.mk
+++ soundsimple/build/src/subdir.mk
@@ -4,20 +4,20 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../test.cpp 
+../src/simplesound.cpp 
 
 OBJS += \
-./test.o 
+./src/simplesound.o 
 
 CPP_DEPS += \
-./test.d 
+./src/simplesound.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-%.o: ../%.cpp
+src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++11 -I/home/ot/.local/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
