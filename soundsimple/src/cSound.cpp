0d30a1c6993aacfc0a00007117e6cdec4ea5ca48
Happuri <happuri@gmx.com>
2014-11-17 14:22:09 +0100

    wip - almost working

--- soundsimple/src/cSound.cpp
+++ soundsimple/src/cSound.cpp
@@ -10,19 +10,20 @@
 
 using namespace std;
 
-cSound::cSound(bool s) : simulation_(s)
+cSound::cSound(bool s) :
+		simulation_(s)
 {
-	_info("constructor");
+	//_info("constructor");
 }
 
 cSound::~cSound() {
-	_dbg3("destructor");
+	//_dbg3("destructor");
 }
 
 void cSound::ProccessRecording(const sf::Int16* Samples, std::size_t SamplesCount, unsigned int SampleRate) {
 	//cout << "Processing chunk of size " << SamplesCount << endl;
-	_fact("Proccessing recording");
-	_info(*Samples << " , count " << SamplesCount);
+	//_fact("Proccessing recording");
+	//_info(*Samples << " , count " << SamplesCount);
 	//unsigned int SampleRate = GetSampleRate();
 
 	// Convert input array to double
@@ -67,17 +68,18 @@ void cSound::ProccessRecording(const sf::Int16* Samples, std::size_t SamplesCoun
 	for (size_t i = 0; i < N; i++) {
 		mag.at(i) /= maxMag;
 
-		if(mag.at(i) >= threshold) _dbg1(mag.at(i) << "\t" << freq.at(i));
+		//if (mag.at(i) >= threshold) _dbg1(mag.at(i) << "\t" << freq.at(i));
 	}
 
 	//Detect Alarm
 
-
 	int detectLow = 4500;
 	int detectHigh = 5500;
 
-	int LowInd = detectLow / ((1.0 / N) * SampleRate); _dbg3("Low: " << LowInd);
-	int HighInd = detectHigh / ((1.0 / N) * SampleRate); _dbg3("High: " << HighInd);
+	int LowInd = detectLow / ((1.0 / N) * SampleRate);
+	//_dbg3("Low: " << LowInd);
+	int HighInd = detectHigh / ((1.0 / N) * SampleRate);
+	//_dbg3("High: " << HighInd);
 
 	vector<double> magRange;
 	vector<double> magX;
@@ -101,26 +103,23 @@ void cSound::ProccessRecording(const sf::Int16* Samples, std::size_t SamplesCoun
 	}
 	_dbg2("energy (sum of mags): " << energy);
 
-	Check(mag, 550, 1200);
-	Check(mag, 2800, 3500);
-	Check(mag, 3770, 42000);
+	confirmations = Interpret(mag, SampleRate, N);
 
-	if (confirmations ) {
+	if (confirmations >= 4) {
 		_mark("alarm (confirmations): " << confirmations);
 
 		std::ofstream log;
 		log.open("log.txt", std::ios::app);
-		cout << currentDateTime() << "  ALARM DETECTED" << endl;
-		log << currentDateTime() << "  ALARM DETECTED" << endl;
+		cout << currentDateTime() << "  ALARM DETECTED: " << confirmations << endl;
+		log << currentDateTime() << "  ALARM DETECTED: " << confirmations << endl;
 		log.close();
 	}
 
-
 	// Plot results
 	bool plot;
-	plot = 1;
+	plot = 0;
 
-	if (plot) {
+	if (plot || simulation_) {
 		try {
 			Gnuplot g1("spectrum");
 
@@ -130,8 +129,9 @@ void cSound::ProccessRecording(const sf::Int16* Samples, std::size_t SamplesCoun
 
 			//g1.set_style("points").plot_xy(magX,magRange,"magnitude");
 
-			if(simulation_) wait_for_key();
-			else sleep(2);
+			if (simulation_) wait_for_key();
+			else
+				sleep(2);
 		}
 		catch (GnuplotException & ge) {
 			cout << ge.what() << endl;
@@ -142,18 +142,68 @@ void cSound::ProccessRecording(const sf::Int16* Samples, std::size_t SamplesCoun
 	fftw_free(in);
 	fftw_free(out);
 
-	cout << "\n\n\n" << endl;
-
 }
-void cSound::Check(const std::vector<double>& mag, const unsigned int& from, const unsigned int& to) {
-	double sum = 0.;
-	for(auto i = from; i<to; ++i) {
-		sum += mag.at(i);
+
+int cSound::Interpret(const std::vector<double> &mag, unsigned int SampleRate, size_t N) {
+	int confLvl = 0;
+
+	const auto range1 = Check(mag, 550, 1200, SampleRate, N);
+
+	if (IsInRange(range1->max, 0.95, 1.))
+		confLvl+=2;
+
+	if(IsInRange(range1->avg, 0.006, 0.012))
+		confLvl++;
+
+
+
+	const auto range2 = Check(mag, 2800, 3500, SampleRate, N);
+	if (IsInRange(range2->max, 0.2, 0.3))
+		confLvl+=2;
+
+	if( IsInRange(range2->avg, 0.002, 0.004))
+		confLvl++;
+
+
+
+	const auto range3 = Check(mag, 3770, 4200, SampleRate, N);
+	if (IsInRange(range3->max, 0.1, 0.2))
+		confLvl+=2;
+
+	if(IsInRange(range3->avg, 0.002, 0.004))
+		confLvl++;
+
+
+	_note("conflvl: " << confLvl);
+
+	return confLvl;
+}
+
+std::shared_ptr<cSound::alarmData> cSound::Check(const std::vector<double> &mag, int from, int to, unsigned int SampleRate, size_t N) {
+	auto range = make_shared<cSound::alarmData>();
+	range->sum = 0.;
+	range->max = 0.;
+
+	assert(from < to);
+
+	from /= (1.0 / N) * SampleRate;
+	to /= (1.0 / N) * SampleRate;
+
+	range->from = from;
+	range->to = to;
+
+	for (auto i = from; i <= to; ++i) {
+		range->sum += mag.at(i);
+
+		if (mag.at(i) > range->max)
+		range->max = mag.at(i);
 	}
-	_note("from: " << from << ", to: " << to << ", sum: " << sum);
+
+	range->avg = range->sum / (to - from);
+	//_note("max: " << range->max << ", avg: " << range->avg << ", sum: " << range->sum);
+	return range;
 
 }
-
 
 void cSound::ProccessRecordingTest(const sf::SoundBuffer Buffer) {
 
@@ -171,6 +221,8 @@ const std::string cSound::currentDateTime() {
 	return buf;
 }
 
+void cSound::Alarm(int level) {
+}
 
 void cSound::wait_for_key() {
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__TOS_WIN__)  // every keypress registered, also arrow keys
@@ -186,4 +238,11 @@ void cSound::wait_for_key() {
 	std::cin.get();
 #endif
 	return;
+}
+
+bool cSound::IsInRange(double var, double from, double to) {
+	if (from > to) return false;
+	if (var >= from && var <= to) return true;
+	else
+		return false;
 }
