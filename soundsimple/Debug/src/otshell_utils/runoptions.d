6a001393f0b4b8326c9dc102b51e1a32c693dd51
Happuri <happuri@gmx.com>
2014-11-17 14:23:37 +0100

    deleting binary files

--- soundsimple/Debug/src/otshell_utils/runoptions.d
+++
@@ -1,6 +0,0 @@
-src/otshell_utils/runoptions.d: ../src/otshell_utils/runoptions.cpp \
- ../src/otshell_utils/runoptions.hpp ../src/otshell_utils/lib_common1.hpp
-
-../src/otshell_utils/runoptions.hpp:
-
-../src/otshell_utils/lib_common1.hpp:
