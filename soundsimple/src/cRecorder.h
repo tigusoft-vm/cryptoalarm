70067149c2df5f8533896115b3ec5bced0598db8
Happuri <happuri@gmx.com>
2014-11-26 18:06:50 +0100

    alarm data

--- soundsimple/src/cRecorder.h
+++ soundsimple/src/cRecorder.h
@@ -29,6 +29,9 @@ class cAlarmSoundRecorder: public sf::SoundBufferRecorder {
 		unsigned int SampleRate = GetSampleRate();
 
 		auto sound = std::make_shared<cSound>(false);
+		sf::SoundBuffer buff = GetBuffer();
+		buff.LoadFromSamples(Samples, SamplesCount, 2, SampleRate);
+
 		auto wasAlarm = sound->ProccessRecording(Samples, SamplesCount, SampleRate);
 
 		if (wasAlarm) saveBuffToFile(Samples, SamplesCount, SampleRate, sound->currentDateTime());
@@ -41,7 +44,7 @@ class cAlarmSoundRecorder: public sf::SoundBufferRecorder {
 	void saveBuffToFile(const sf::Int16* Samples, std::size_t SamplesCount, unsigned int SampleRate, std::string filename) {
 		filename += ".wav";
 		sf::SoundBuffer buff = GetBuffer();
-		buff.LoadFromSamples(Samples, SamplesCount, 2, SampleRate);
+
 		_info("samples count: " << buff.GetSamplesCount() << ", duration: " << buff.GetDuration());
 		if(!buff.SaveToFile(filename)) _erro(filename << " not saved :( ");
 	}
