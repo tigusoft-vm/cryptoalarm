10a0d2c88ec952d1a1e9a7ef8f42e3a62b71e5e3
Robert <robert-bitmonero-dev@mail.l>
2014-11-26 18:32:46 +0100

    compilation errors

--- soundsimple/src/cAlarmData.cpp
+++ soundsimple/src/cAlarmData.cpp
@@ -5,7 +5,7 @@
  *      Author: abby
  */
 
-#include <cAlarmData.h>
+#include "cAlarmData.h"
 
 cAlarmData::~cAlarmData()
 {
@@ -19,7 +19,8 @@ cAlarmData::cAlarmData()
 }
 
 void cAlarmData::add(const sf::Int16* Samples, std::size_t SamplesCount, unsigned int SampleRate) {
-	auto SamplesCopy = new sf::Int16(Samples);
+	auto SamplesCopy = new sf::Int16 [sizeof Samples]; //(Samples);
+	std::memcpy(SamplesCopy, Samples, sizeof Samples);
 	this->Samples.push_back(*&SamplesCopy);
 	this->SamplesCounts.push_back(SamplesCount);
 	this->SampleRates.push_back(SampleRate);
