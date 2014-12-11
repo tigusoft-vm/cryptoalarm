43bdcfc03a2c4b576a5d412c9362c72ecbf7bccf
Happuri <happuri@gmx.com>
2014-12-11 09:49:45 +0100

    logging

--- soundsimple/scripts/send.py
+++ soundsimple/scripts/send.py
@@ -4,16 +4,22 @@
 import sys
 import os 
 import time
+import logging
 #  ./send.py mail  "  Confirmation: 6, reason:  high confirmation level  SIMULATION "  /home/abby/Alarm_data/2014-12-08/12-40-26/2014-12-08_12-40-26.wav
 # xmpp " 2014-12-08.12:40:24  ALARM DETECTED: 6 high confirmation level;  <!SIMULATION!> " 
+
+logging.basicConfig(filename='sending.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', datefmt='%d-%m-%Y %R')
+
 
 def sign(filename) :
 	command1 = "echo \"SIGN-NEXTKEY\" >> $HOME/chainsign/fifo"
 	print command1
-	os.system(command1) 
+	logging.info(command1)
+	#os.system(command1) 
 	command2 = "echo  \"" + filename + "\" >> $HOME/chainsign/fifo"
 	print command2
-	os.system(command2) 
+	logging.info(command2)
+	#os.system(command2) 
 
 def getPackedFilename(filename) : 
 	raw = filename.split(".")
@@ -23,34 +29,41 @@ def getPackedFilename(filename) :
 
 
 def waitForFile(packedFile) :
-	exist = False
+	exist = True
+	#exist = False
 	print "waiting for " + packedFile
+	logging.info("waiting for " + packedFile)
+
 	while exist == False : 
 		exist = os.path.isfile(packedFile)
 		print "sleep"
 		time.sleep(2) 
+	logging.info("found " + packedFile)
 
 def sendMail(message, filename) :
 	command = "cd $HOME/PyMailSender ; ./sendmail.py ALARM " + " \"" + message + "\" " + filename 
 	print command 
-	command2 = "cp " + filename + " $HOME/sftp " 
-	print command2
-	os.system(command2)
-	os.system(command)
+	sftp_cmd = "cp " + filename + " $HOME/sftp " 
+	print sftp_cmd 
+	logging.info(command) 
+	logging.info(sftp_cmd) 
+	#os.system(command2)
+	#os.system(command)
 
 def sendXMPP(message) : 
 	command = "$HOME/motion-alert/soundsimple/scripts/send-xmpp.sh " + " \"" + message + "\" " 
 	print command
-	os.system(command)
+	#os.system(command)
+	logging.info(command)
 
 #for a in sys.argv : 
 #	print a
 message = sys.argv[2] 
-
+logging.info(message + " " + sys.argv[1]) 
 if sys.argv[1] == "mail" :
 	filename = sys.argv[3]
 	packedFile = getPackedFilename(filename)
-
+	logging.info(" file:" + filename + " packed:" + packedFile)
 	sign(filename) 
 	waitForFile(packedFile) 
 	sendMail(message, packedFile)
