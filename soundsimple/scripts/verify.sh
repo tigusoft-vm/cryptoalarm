bfe91b091fa8c720838d06d27d048ee6bf618d26
rob <rob@mail.l>
2015-06-08 17:36:01 +0200

    verify script

--- soundsimple/scripts/verify.sh
+++ soundsimple/scripts/verify.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 for f in  $(find ~/Alarm_data/ -name '*.sig*' -or -name '*.wav' -or -name '*.pub')
-	do cp $f ~/Alarm_data/
+	do
+	cp $f ~/Alarm_data/
+done
+
+cd $HOME/motion-alert/soundsimple/src/
+
+for f in  ~/Alarm_data/*.sig
+	do
+	./simplesound -v "$f"
+	if [ "$?" -ne "1" ] ; then
+		echo "*********VERIFICATION ERROR*********"
+		break
+	fi
 done
