9e95967baa35224c45f6608c938a28b0e3a984de
Happuri <happuri@gmx.com>
2015-06-17 13:40:39 +0200

    Starting chainsign daemon in video mode

--- do
+++ do
@@ -1,5 +1,60 @@
 #!/bin/bash
+cmd="./do"
+if [[ $0 != $cmd ]]; then 
+	echo -e "${w}Wrong path! You should run this script: ./do ${nc}"
+	exit 1
+fi
+
+head=`pwd`
 git submodule update --init --recursive
-cd soundsimple/
-mkdir -p build/ 
-cd build && cmake .. && make run
+
+w="\033[0;33m"
+c="\033[1;34m"
+nc="\033[0m"
+
+function sound_build() {
+	echo -e "${c}Building soundsimple${nc}"
+	cd soundsimple/
+	mkdir -p build/ 
+	cd build && cmake .. && make run
+	cd $head
+}
+
+function chainsign_build() { 
+	echo -e "${c}Building chainsign${nc}"
+
+	cd chainsign/
+	cmake . && make
+	chain_path=`pwd`
+	echo "$chain_path" > chainsign.conf
+
+	# generate config 
+	cd $head
+}
+
+
+function handle_param() { 
+	if [[ "${1}" == "-h" || "${1}" == "--help" ]] ; then
+		echo -e "Build all:\n\t $cmd"
+		echo -e "Build video:\n\t $cmd video"
+		echo -e "Build soundsimple:\n\t $cmd soundsimple "
+		exit 1
+	elif [[ "${1}" == "soundsimple" ]]; then
+		chainsign_build
+		sound_build
+
+	elif [[ "${1}" == "video" ]]; then
+		chainsign_build
+	fi
+}
+
+
+if [[ $# > 0 ]]; then 
+	handle_param ${1}
+else 
+	echo -e "${c}Build all${nc}"
+	chainsign_build
+	sound_build
+fi
+#chainsign_build
+#sound_build
--- video/motion-start.sh
+++ video/motion-start.sh
@@ -1,21 +1,62 @@
 #!/bin/bash
 date=$(date +%Y-%m-%d_%H:%M:%S%z)
+start_dir=`pwd`
 
-echo "guarding-since-$date[$1]" > ~/stat
+function ask() { 
+        REPLY=$1
+        if ! [[ $REPLY =~ ^[Yy]$ ]]; then
+                echo "Aborting."
+                exit 0
+        fi
+}
 
-./lib_sendxmpp.sh "rfree.mobile@jit.si" "starting..."
-sleep 10
-./lib_sendxmpp.sh "rfree.mobile@jit.si" "starting..."
-sleep 60
+function start_chainsign_daemon() { 
+	echo "[INFO] Starting chainsign daemon" 
+	if ! [ -d ../chainsign ]; then
+		echo "[ERROR] chainsign directory doesn't exist"
+		echo "Run ./do script or(and) check directory"
+	fi	
 
-./lib_sendxmpp.sh "rfree.mobile@jit.si" "starting...3"
-sleep 5
-./lib_sendxmpp.sh "rfree.mobile@jit.si" "starting...2"
-sleep 5
-./lib_sendxmpp.sh "rfree.mobile@jit.si" "starting...1"
-sleep 5
-./lib_sendxmpp.sh "rfree.mobile@jit.si" "starting...GO"
-sleep 5
+	cd ../chainsign
+
+	if ! [ -f chainsign ]; then
+		cmake . && make
+	fi
+
+	./chainsign --daemon
+
+}
+
+
+if [[ $0 != "./motion-start.sh" ]]; then 
+	echo "[WARN] This script should be called using command: ./motion-start"
+	echo "[WARN] Continue? "
+	read yn
+	ask $yn
+fi
+
+if [[ $1 != "--dry-run" ]]; then
+	start_chainsign_daemon 
+
+	echo "guarding-since-$date[$1]" > ~/stat
+
+	./lib_sendxmpp.sh "rfree.mobile@jit.si" "starting..."
+	sleep 10
+	./lib_sendxmpp.sh "rfree.mobile@jit.si" "starting..."
+	sleep 60
+
+	./lib_sendxmpp.sh "rfree.mobile@jit.si" "starting...3"
+	sleep 5
+	./lib_sendxmpp.sh "rfree.mobile@jit.si" "starting...2"
+	sleep 5
+	./lib_sendxmpp.sh "rfree.mobile@jit.si" "starting...1"
+	sleep 5
+	./lib_sendxmpp.sh "rfree.mobile@jit.si" "starting...GO"
+	sleep 5 
+fi
+
 motion
-./lib_sendxmpp.sh "rfree.mobile@jit.si" "end of program"
 
+if [[ $1 != "--dry-run" ]]; then
+	./lib_sendxmpp.sh "rfree.mobile@jit.si" "end of program"
+fi
