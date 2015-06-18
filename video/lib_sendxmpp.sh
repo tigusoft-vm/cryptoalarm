10c9756569f09f4ab75d818a5f91e8c2a69933e6
rfree2 <rfree@op.pl>
2015-06-18 15:51:16 +0200

    Motion scripts - wip

--- video/lib_sendxmpp.sh
+++ video/lib_sendxmpp.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+echo "XMPP sending is DISABLED, not sending (in $0)" ; exit 0
+
 to=$1
 shift
 echo " $@" | sendxmpp -v $to
--- video/on_event_start.sh
+++ video/on_event_start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "EVENT in $0, options are: $*"
+echo "EVENT in $1, options are also: $1 $2 $3 $4"
+
 date=$(date +%Y-%m-%d_%H:%M:%S%z)
 echo "DETECTED (on $date)" > ~/status
 bash lib_event.sh "DETECTED" "alertalert"
