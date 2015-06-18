7f0a0967d21378b1b9a2d3573f314495d73711f8
rfree2 <rfree@op.pl>
2015-06-18 18:41:00 +0200

    this version should sign video WARNIGN! errors!
    errors/bugs remain in code.

--- video/on_movie_end.sh
+++ video/on_movie_end.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 #$1 = video file
+
 head=`pwd`
+
+echo "EVENTSCRIPT: $0... will sign the file. head=$head"
+
+currpwd="$PWD"
+filepwd="$currpwd"
 cd ../chainsign
-./sign_file.sh $1
-mv *.pub "$head/rec"
-mv *.sig "$head/rec"
+echo "Calling chainsign - start"
+./sign_file.sh "$filepwd/$1"
+echo "Calling chainsign - done"
+mv *.pub "$head/rec/"
+mv *.sig "$head/rec/"
+cd "$currpwd"
 
 bash lib_event.sh "recdone" "info"
+
